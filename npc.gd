b5fac1dc5e4c528046c4c297ab8f81f3bee23818
Nocturnal <214055938+Nocturnal-sys@users.noreply.github.com>
2025-07-15 02:58:05 +0100

    create environment to interact with

---
+++ npc.gd
@@ -0,0 +1,5 @@
+extends StaticBody2D
+class_name npc
+@onready var this_sprite: Sprite2D = $sprite
+
+@export var sprite = this_sprite
---
+++ player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+const SPEED = 160.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	if direction:
+		velocity = direction.normalized() * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()

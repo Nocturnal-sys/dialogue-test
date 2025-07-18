9368f7bf0afd67d69fdfd4d539e7bc1ebf3d4459
Nocturnal <214055938+Nocturnal-sys@users.noreply.github.com>
2025-07-18 16:43:52 +0100

    add dialogue system with animated text box

--- npc.gd
+++ npc.gd
@@ -1,5 +1,32 @@
 extends StaticBody2D
-class_name npc
+class_name Npc
 @onready var this_sprite: Sprite2D = $sprite
+@onready var down: RayCast2D = $Down
+@onready var left: RayCast2D = $Left
+@onready var right: RayCast2D = $Right
+@onready var up: RayCast2D = $Up
+@onready var path: PathFollow2D = $Path2D/PathFollow2D
+@onready var e: Sprite2D = $Path2D/PathFollow2D/E
+@onready var npc: Npc = $"."
 
-@export var sprite = this_sprite
+@export var lines: Array[String]
+
+var is_colliding := false
+var reverse_e := false
+
+
+func _process(delta: float) -> void:
+
+	path.progress_ratio += 1 * delta
+
+	if down.is_colliding() or up.is_colliding() or left.is_colliding() or right.is_colliding():
+		e.visible = true
+		is_colliding = true
+	else:
+		e.visible = false
+		is_colliding = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		if is_colliding:
+			DialogueManager.start_dialogue(global_position, lines)
--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 160.0
+const SPEED = 140.0
 const JUMP_VELOCITY = -400.0
 
 
@@ -9,7 +9,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	var direction := Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	if direction:
 		velocity = direction.normalized() * SPEED
 	else:
